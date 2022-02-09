a58871503c4d48202df9c6bf60dc1de2a1ab73ca
Norman Yard <67605918+noyard@users.noreply.github.com>
2022-02-09 14:13:18 -0600

    112 commit

---
+++ PowerStigCreateChecklist.ps1
@@ -0,0 +1,145 @@
+﻿Param(
+	[Parameter(Mandatory=$true, HelpMessage="The location of the MOF files to create checklists")]
+	[string]$MofLocation
+)
+
+#Locations for folder structures
+$ScanResults       = ".\Results"
+$ChecklistLocation = ".\Checklists"
+$ManualExceptions  = ".\ManualExceptions"
+$DISAStigs         = ".\Stigs"
+$LogLocation       = ".\Logs"
+
+#Prepare Log folder and start logging.
+If (!(Test-Path $LogLocation))		{[void](New-Item $LogLocation -itemType Directory)}
+get-childitem $loglocation -Recurse | where{-not $_.PsIsContainer}| sort CreationTime -desc | Select -Skip 10 |Remove-Item -Force
+Start-Transcript -Path "$($LogLocation)\Log_$(get-date -format yyyyMMdd-HHmmss).log"
+
+#Make sure folder structures exist. 
+if (!(Test-Path $ScanResults))		{[void](New-Item $ScanResults -itemType Directory)}
+if (!(Test-Path $ChecklistLocation)){[void](New-Item $ChecklistLocation -itemType Directory)}
+if (!(Test-Path $ManualExceptions)) {[void](New-Item $ManualExceptions -itemType Directory)}
+if (!(Test-Path $DISAStigs))		{[void](New-Item $DISAStigs -itemType Directory)}
+
+# Make sure all XCCDF files are not blocked becuase they were possibly downloaded from the internet. 
+dir $DISAStigs -Recurse |Unblock-File
+
+#get the *.mof files to process
+if (!(test-path -Path $MofLocation)) {"---MOF file Location does not exist - $($MofLocation)"; Stop-Transcript ;Exit}
+$MofFiles = Get-ChildItem -Path $MofLocation -Filter "*.mof"-Exclude "*.meta.mof"-Recurse -ErrorAction SilentlyContinue| %{$_.FullName}
+"$($MofFiles.count) MOF File to process"
+
+#Import functions
+. .\PowerStigCreateChecklistFunctions.ps1
+
+#Process MOF files
+Foreach ($file in $MofFiles)	 
+	{
+		"`r`nProcessing $($File)"
+		##Find Target of MOF
+		$TargetNode = (Get-MOFMetadata $file).TargetNode	
+		"Testing to see if $($TargetNode) is online"
+		if(Test-Connection -ComputerName $($TargetNode) -Quiet -Count 1)
+			{
+			"Running MOF against $($TargetNode)"
+			$DscResult = Test-DscConfiguration -ComputerName $TargetNode -path $MofLocation
+			If ($DscResult) 
+				{
+				"Test Configuration was successful"
+				$DscResult | Export-Clixml "$($ScanResults)\$($TargetNode).xml"
+				$DscResultRehydrated = Import-Clixml "$($ScanResults)\$($TargetNode).xml"
+
+				##Find Stigs in the MOF
+				$Stigs = @()
+				$lines = get-content $file |where {$_ -match "ResourceID = "}
+				foreach ($line in $lines)
+					{
+					$r = [regex] "\[([^\[]*)\]"
+					$match = $r.match($line.split("::")[2])
+					$Stigs += $match.groups[1].value
+					}
+				$stigs = $stigs  | Where { $_ -match '\S' }| select -uniq
+
+				if($Stigs -contains "WindowsServer")
+					{
+					try {
+						$ServerOS = $((get-wmiobject -class win32_operatingsystem -computer $($TargetNode)).name)
+						#$DomainController = get-service -name "kdc" -ComputerName "cmdc01" -ErrorAction 0
+						switch -wildcard ($ServerOS) {
+							"*Windows Server 2016*" {$Stigs = $Stigs -replace "WindowsServer","WindowsServer2016"}
+							"*Windows Server 2019*" {$Stigs = $Stigs -replace "WindowsServer","WindowsServer2019"}
+							"*Windows Server 2022*" {$Stigs = $Stigs -replace "WindowsServer","WindowsServer2022"}
+							Default {"---Unable to determine OS"}
+							}
+						}catch {
+						"---Error Unable to query Operating System on $($TargetNode)"
+						}	
+					}
+
+				if($Stigs -contains "IisServer" -or $Stigs -contains "IisSite")
+					{
+					try {
+						$reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $($TargetNode))
+						$regkey = $reg.OpenSubkey("SOFTWARE\\Microsoft\\InetStp")
+						$IISVersion = $regkey.GetValue("VersionString")
+						$IISVersion
+						
+						switch -wildcard ($IISVersion) {
+							"*8.5*" {$Stigs = $Stigs -replace "IisServer","IisServer85";$Stigs = $Stigs -replace "IisSite","IisSite85"}
+							"*10*" {$Stigs = $Stigs -replace "IisServer","IisServer10";$Stigs = $Stigs -replace "IisSite","IisSite10"}
+							Default {"---Unable to determine IIS Version"}
+							}
+						}catch {
+						"---Error Unable to query registry on $($TargetNode) to determine IIS Version"
+						}	
+					}
+
+				##Remove known DSC items that do not contain STiG items.
+				$Stigs = $Stigs | Where-Object { $_ -ne "SharePointServerEnforceTLS12" }
+
+				##Find the DISA STiG Files for each Stig
+				$XCCDFPath = @()
+				$ManualCheckFile = @()
+				[void](New-Item -Path '$($ScanResults )\' -Name "$($TargetNode).txt" -ItemType File -Force)
+				foreach ($Stig in $Stigs)
+					{
+						"MOF has $($Stig) Stig"
+						if (!(Test-Path "$($DISAStigs)\$($Stig)")){[void](New-Item "$($DISAStigs)\$($Stig)" -itemType Directory)}
+						Get-ChildItem "$($DISAStigs)\$($Stig)" -Filter *xccdf.xml |
+						Foreach-Object { 
+							"Adding $($_.FullName) to Stig list"
+							Add-Content -Path "$($ScanResults )\$($TargetNode).txt" -Value "$($_.FullName)" 
+						}
+						if(Test-path "$($DISAStigs)\$($Stig)\ManualExceptions.xml")
+							{
+								Add-Content -Path "$($ScanResults )\$($TargetNode)_Manual.txt" -Value "$($DISAStigs)\$($Stig)\ManualExceptions.xml"
+							}
+					}
+				if (Test-Path "$($ManualExceptions)\$($TargetNode).xml")
+					{
+						Add-Content -Path "$($ScanResults )\$($TargetNode)_Manual.txt" -Value "$($ManualExceptions)\$($TargetNode).xml"
+					}
+					
+				$XccdfPath = Get-Content "$($ScanResults )\$($TargetNode).txt"
+				$ManualCheckFile =  Get-Content "$($ScanResults )\$($TargetNode)_Manual.txt"
+
+				"Generating Checklist"
+				if( $ManualCheckFile.Count -ge 1)
+					{
+					New-StigCheckList -DscResult $DscResultRehydrated -XccdfPath $XCCDFPath -ManualChecklistEntriesFile $ManualCheckFile -OutputPath "$($ChecklistLocation)\$($TargetNode).ckl"
+					}
+				else {
+					New-StigCheckList -DscResult $DscResultRehydrated -XccdfPath $XCCDFPath -OutputPath "$($ChecklistLocation)\$($TargetNode).ckl"
+					}
+			}
+			Else {
+				"Unable to test configuration"
+			}
+		}
+		Else {
+			"---$($TargetNode) is offline"	
+		}
+		"Processing $($File) Completed"
+    } 
+"`r`nScript Completed"
+Stop-Transcript
---
+++ PowerStigCreateChecklistFunctions.ps1
@@ -0,0 +1,67 @@
+﻿Function Get-MOFMetadata {
+	[cmdletbinding()]
+	Param(
+		[Parameter(Position=0,
+		Mandatory,
+		HelpMessage="Enter the path to a MOF file",
+		ValueFromPipeline,
+		ValueFromPipelineByPropertyName)]
+		[ValidateNotNullorEmpty()]
+		[ValidateScript({Test-Path $_})]
+		[Alias("PSPath")]
+		[string]$Path
+	)
+	Begin {
+		Write-Verbose "Starting $($MyInvocation.Mycommand)"
+	} #begin
+	Process {
+		Write-Verbose "Processing $path"
+		#read the MOF file into a variable
+		$content = Get-Content -Path $Path -ReadCount 0
+		#create an ordered hashtable
+		$hashProperties = [ordered]@{}
+		#get first 4 lines
+		Write-Verbose "Getting comment header"
+		$meta = $content | Select -Skip 1 -first 4
+		foreach ($item in $meta) {
+			#split each line
+			$split = $item.split("=")
+			$Name = $split[0].Replace("@","")
+			$value = $split[1].Replace("'","")
+			#test if a value is a datetime
+			[ref]$r = Get-Date
+			if ([datetime]::TryParse($value,$r)) {
+				#replace value with $r which will now be the
+				#value from the MOF treated as a datetime object
+				$value = $r.value
+			}
+			#add each element to the hashtable removing extra characters
+			$hashProperties.Add($name,$value)
+		}
+		#get version information
+		#getting more context than necessary in case you want to include
+		#other information
+		$OMIDoc = $Content| Select-String "OMI_ConfigurationDocument" -Context 6 |
+		Select -ExpandProperty Context
+		#get version string
+		if (($OMIDoc.PostContext | Select-String version).ToString().trim() -match "\d+\.\d+\.\d") {
+			$Version = $($matches.Values[0])
+		}
+		else {
+			$version = "Unknown"
+		}
+		$hashProperties.add("Version",$Version)
+		#add file information
+		Write-Verbose "Getting file information"
+		$file = Get-Item -Path $Path
+		$hashProperties.Add("LastModified",$file.LastWriteTime)
+		$hashProperties.Add("Name",$file.name)
+		$hashProperties.Add("Size",$file.Length)
+		$hashProperties.Add("Path",$file.FullName)
+		Write-Verbose "Creating output object"
+		New-Object -TypeName PSObject -Property $hashProperties
+	} #end process
+	End {
+		Write-Verbose "Ending $($MyInvocation.Mycommand)"
+	} #end
+} #end Get-MOFMetadata
